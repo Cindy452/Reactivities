b753b699cf5a5fa04517fc8a2a46c99ef99663db
Your Name <you@example.com>
2020-12-01 15:54:43 -0600

    End of section 8

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './app/layout/style.css';
 import App from './app/layout/App';
+import {BrowserRouter} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
+import ScrollToTop from './app/layout/ScrollToTop';
 
-ReactDOM.render( <App />, document.getElementById('root'));
+ReactDOM.render( 
+    <BrowserRouter>
+    <ScrollToTop />
+    <App /> 
+    </BrowserRouter>,
+document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
