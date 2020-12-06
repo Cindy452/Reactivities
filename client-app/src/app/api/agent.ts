8e2c6c8b5d23c922d22acd771265dec1041a95bd
Your Name <you@example.com>
2020-12-06 13:36:58 -0600

    End of section 17: Client-upload photo

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,7 +2,8 @@ import axios, { AxiosResponse } from 'axios';
 import { IActivity } from '../models/activity';
 import { history } from '../..';
 import { toast } from 'react-toastify';
-import {IUser, IUserFormValues} from '../models/user';
+import { IUser, IUserFormValues } from '../models/user';
+import { IProfile, IPhoto } from '../models/profile';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
@@ -40,7 +41,14 @@ const requests = {
     get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
     post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
     put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
-    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody) 
+    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
+    postForm: (url: string, file: Blob) => {
+        let formData = new FormData();
+        formData.append('File', file);
+        return axios.post(url, formData, {
+            headers: {'Content-type': 'multipart/form-data'}
+        }).then(responseBody)
+    }
 };
 
 const Activities = {
@@ -56,11 +64,19 @@ const Activities = {
 const User = {
     current: (): Promise<IUser> => requests.get('/user'),
     login: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/login`, user),
-    register: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/register`, user)
+    register: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/register`, user),
 }
 
-// eslint-disable-next-line
+const Profiles = {
+    get: (username: string): Promise<IProfile> => requests.get(`/profiles/${username}`),
+    uploadPhoto: (photo: Blob): Promise<IPhoto> => requests.postForm(`/photos`, photo),
+    setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+    deletePhoto: (id: string) => requests.del(`/photos/${id}`)
+}
+
+ // eslint-disable-next-line
 export default {
     Activities,
-    User
+    User,
+    Profiles
 }
